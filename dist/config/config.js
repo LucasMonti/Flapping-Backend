69dbfd6daba55303c4d115652f6760ba2e907ad4
lucasmon <lucasmon@amdocs.com>
2022-02-14 17:52:43 -0300

    antes de apertura de rama individual

---
+++ dist/config/config.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Configuracion = void 0;
+class Configuracion {
+}
+exports.Configuracion = Configuracion;
+//# sourceMappingURL=config.js.map
